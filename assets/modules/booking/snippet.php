98b0e73b9ac09e2cf010caf0fcd9868f3a8819df
GTX59 <gtx59@yandex.ru>
2021-02-09 14:36:08 +0500

    Update snippet.php

--- assets/modules/booking/snippet.php
+++ assets/modules/booking/snippet.php
@@ -6,15 +6,29 @@ while($row=$modx->db->getRow($res))$opt=json_decode($row['properties'],true);
 $holidays=$opt['holidays'][0]['value'];
 $weekends=$opt['weekends'][0]['value'];
 $plusdays=$opt['plusdays'][0]['value'];
-$intervals=$opt['intervals'][0]['value'];
+$intervals1=$opt['intervals1'][0]['value'];
+$intervals2=$opt['intervals2'][0]['value'];
+$intervals3=$opt['intervals3'][0]['value'];
+$intervals4=$opt['intervals4'][0]['value'];
+$intervals5=$opt['intervals5'][0]['value'];
+$intervals6=$opt['intervals6'][0]['value'];
+$intervals7=$opt['intervals7'][0]['value'];
+
 $scripts='
 <script src="assets/modules/booking/pickmeup.min.js"></script>
 <script>
 	var MODX_SITE_URL="'.MODX_SITE_URL.'";
 	var holidays = "'.$holidays.'";
 	var weekends = "'.$weekends.'";
-	var plusdays = '.$plusdays.'+0;
-	var intervals = "'.$intervals.'";
+	var plusdays = parseInt('.$plusdays.');
+	let intervals = [];
+	intervals[1] = "'.$intervals1.'";
+	intervals[2] = "'.$intervals2.'";
+	intervals[3] = "'.$intervals3.'";
+	intervals[4] = "'.$intervals4.'";
+	intervals[5] = "'.$intervals5.'";
+	intervals[6] = "'.$intervals6.'";
+	intervals[0] = "'.$intervals7.'";
 	var calendars = 1;
 	var manager=false;
 </script>
